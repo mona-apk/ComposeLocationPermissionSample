7be190f9bdedf3c5de803076d6a20e5b4110e379
mona-apk <cordion_nap@yahoo.co.jp>
2022-11-24 20:16:28 +0900

    feat: call LocationRequestScreen

--- app/src/main/java/com/apk/mona/composelocationpermissionsample/MainActivity.kt
+++ app/src/main/java/com/apk/mona/composelocationpermissionsample/MainActivity.kt
@@ -21,7 +21,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLocationPermissionSampleTheme {}
+            ComposeLocationPermissionSampleTheme {
+                LocationRequestScreen()
+            }
         }
     }
 }
