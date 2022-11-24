dd4b51c82ba4ab62ad5a79084432e86c552e3da6
mona-apk <cordion_nap@yahoo.co.jp>
2022-11-24 18:45:35 +0900

    feat: create LocationRequestScreen

--- app/src/main/java/com/apk/mona/composelocationpermissionsample/MainActivity.kt
+++ app/src/main/java/com/apk/mona/composelocationpermissionsample/MainActivity.kt
@@ -3,38 +3,56 @@ package com.apk.mona.composelocationpermissionsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.apk.mona.composelocationpermissionsample.ui.theme.ComposeLocationPermissionSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLocationPermissionSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            ComposeLocationPermissionSampleTheme {}
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LocationRequestScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier.systemBarsPadding(),
+    ) { insetsPadding ->
+        LocationRequestPage(
+            onClick = {},
+            modifier = Modifier.padding(insetsPadding)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeLocationPermissionSampleTheme {
-        Greeting("Android")
+fun LocationRequestPage(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Button(
+            onClick = onClick,
+            modifier = Modifier.align(Alignment.Center),
+        ) {
+            Text(
+                text = "TAP!",
+                color = Color.White,
+            )
+        }
     }
 }
